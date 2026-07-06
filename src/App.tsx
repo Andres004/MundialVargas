a58b766c9eb70efffd4ccb367b740e829107cb63
unknown <ricardo.andrade@ucb.edu.bo>
2026-07-05 20:44:52 -0400

    a

--- src/App.tsx
+++ src/App.tsx
@@ -25,9 +25,9 @@ const PARTIDOS_DE_HOY = [
     away_team: 'Noruega', 
     home_flag: 'https://flagcdn.com/w80/br.png', 
     away_flag: 'https://flagcdn.com/w80/no.png', 
-    home_score: 0, 
-    away_score: 0, 
-    status: 'PENDING', 
+    home_score: 1, 
+    away_score: 2, 
+    status: 'FINISHED', 
     time: '16:00',
     advanced_team: '' 
   },
@@ -40,7 +40,7 @@ const PARTIDOS_DE_HOY = [
     home_score: 0, 
     away_score: 0, 
     status: 'PENDING', 
-    time: '20:00',
+    time: '21:00',
     advanced_team: '' 
   }
 ];
